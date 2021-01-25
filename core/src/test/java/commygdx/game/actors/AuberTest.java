da2ea9d3a489d0cf9aafaebe9ab8a5addcfa785c
Lizzy <lizzy.howells99@gmail.com>
2021-01-25 16:00:03 +0000

    Testing Setup

---
+++ core/src/test/java/commygdx/game/actors/AuberTest.java
@@ -0,0 +1,27 @@
+package test.java.commygdx.game.actors;
+
+
+import org.junit.jupiter.api.Test;
+
+class AuberTest {
+
+    @Test
+    void getTexture() {
+    }
+
+    @Test
+    void handleMovement() {
+    }
+
+    @Test
+    void teleportCheck() {
+    }
+
+    @Test
+    void arrest() {
+    }
+
+    @Test
+    void shuffle() {
+    }
+}
